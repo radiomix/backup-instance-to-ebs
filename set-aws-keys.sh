4a466cbe9386307cf439a7890ad72986f7064b05
Admin User <mkloeckner@ip-10-113-153-21.ec2.internal>
2015-12-15 16:36:09 +0000

    add empty file to set aws credentials

---
+++ set-aws-keys.sh
@@ -0,0 +1,8 @@
+echo " Usage: source set-aws-keys.sh "
+echo "Sets $(whoami) AWS credentials in Account DEV"
+export export AWS_ACCOUNT=
+export AWS_ACCESS_KEY=
+export AWS_SECRET_KEY=
+export AWS_ACCOUNT_ID=
+export aws_access_key_id=
+export aws_access_secret_key=
